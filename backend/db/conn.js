fac293931003a3b657df9791eb03ede24dbd506f
Suhas <kamblesuhas12@gmail.com>
2021-12-17 19:38:25 +0530

    initial commit

--- backend/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://suhas:suhas@cluster0.cj8de.mongodb.net/Blog?retryWrites=true&w=majority")
-.then(()=>{
-  console.log("Database is connected...");
-})
-.catch((e)=>{
-  console.log("Database is not connected...");
-})
